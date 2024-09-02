79c10c4f0e2af5bd5cdfff9435d631c1e1bca573
haywise <152945861+haywise@users.noreply.github.com>
2024-09-02 10:04:03 -0500

    Update ec2.tf

--- NEXUS/ec2.tf
+++ NEXUS/ec2.tf
@@ -108,5 +108,5 @@ resource "aws_instance" "ec2_instance" {
 
 # print the url of the jenkins server
 output "website_url" {
-  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8081"])
+  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns])
 }
